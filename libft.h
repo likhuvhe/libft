5a73f7f1aecdc6c4d641a5de5c04466a160a3936
Lufuno Israel KHUVHE <lkhuvhe@c4r8s2.wethinkcode.co.za>
2019-05-21 10:22:38 +0200

    libft file

--- libft.h
+++ libft.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lkhuvhe <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/20 15:02:56 by lkhuvhe           #+#    #+#             */
+/*   Updated: 2019/05/21 10:10:49 by lkhuvhe          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LIBFT_H
 #define HEADER_H
 
@@ -8,5 +20,7 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+size_t	ft_strlen(const char *s);
 
 #endif
