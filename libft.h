306980a1a8ea865c5e1a5bc44d6e63cd9c494782
Lufuno Israel KHUVHE <lkhuvhe@c4r8s2.wethinkcode.co.za>
2019-05-21 11:53:08 +0200

    libft

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lkhuvhe <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 15:02:56 by lkhuvhe           #+#    #+#             */
-/*   Updated: 2019/05/21 10:10:49 by lkhuvhe          ###   ########.fr       */
+/*   Updated: 2019/05/21 11:52:19 by lkhuvhe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,6 @@ void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 size_t	ft_strlen(const char *s);
+char	*ft_strcpy(char *dst, const char *src);
 
 #endif
