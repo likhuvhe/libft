d8a1ebfa20700744eb82d5db5e86a1886c26337a
Lufuno Israel KHUVHE <lkhuvhe@c4r8s2.wethinkcode.co.za>
2019-05-21 15:40:16 +0200

    libft

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: lkhuvhe <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 17:01:12 by lkhuvhe           #+#    #+#             */
-/*   Updated: 2019/05/21 15:09:25 by lkhuvhe          ###   ########.fr       */
+/*   Updated: 2019/05/21 15:38:34 by lkhuvhe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,12 @@
 void	*ft_memchr(const void *s, int c, size_t n)
 {
 	unsigned char *str;
-	
+
 	str = (unsigned char*)s;
 	while (n != 0)
 	{
 		if (*str++ == (unsigned char)c)
 		{
-
 			return (str - 1);
 		}
 		n--;
