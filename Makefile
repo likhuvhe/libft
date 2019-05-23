f0fee572c01e075b5f510cb54632af2c9439ba8c
Lufuno Israel KHUVHE <lkhuvhe@c4r8s2.wethinkcode.co.za>
2019-05-23 10:53:32 +0200

    libft

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 NAME	= libft.a
 
 SRC	= ft_memset.c ft_bzero.c ft_memcpy.c ft_memccpy.c ft_memmove.c ft_memchr.c ft_strlen.c ft_strcpy.c ft_strncpy.c ft_strcat.c\
-	  ft_atoi.c ft_isalpha.c ft_isdigit.c test.c
+	  ft_atoi.c ft_isalpha.c ft_isdigit.c ft_isalnum.c test.c
 
 OBJ	= $(SRC:.c=.o)
 CFLAGS	= -Wall -Wextra -Werror
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lkhuvhe <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 15:02:56 by lkhuvhe           #+#    #+#             */
-/*   Updated: 2019/05/23 10:02:39 by lkhuvhe          ###   ########.fr       */
+/*   Updated: 2019/05/23 10:31:13 by lkhuvhe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,6 @@ char	*ft_strcat(char *restrict s1, const char *restrict s2);
 int		ft_atoi(const char *str);
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
+int		ft_isalnum(int c);
 
 #endif
