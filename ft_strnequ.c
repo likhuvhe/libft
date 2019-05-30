6697621de071adc51f3c3f7ef3efa673258948fb
Lufuno Israel KHUVHE <lkhuvhe@c4r8s2.wethinkcode.co.za>
2019-05-30 18:03:36 +0200

    ft_strnequ.c

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,13 +6,13 @@
 /*   By: lkhuvhe <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/30 17:31:34 by lkhuvhe           #+#    #+#             */
-/*   Updated: 2019/05/30 17:46:13 by lkhuvhe          ###   ########.fr       */
+/*   Updated: 2019/05/30 18:01:42 by lkhuvhe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int  ft_strnequ(char const *s1, char const *s2,size_t n)
+int		ft_strnequ(char const *s1, char const *s2, size_t n)
 {
 	if (!s1 || !s2)
 		return (0);
