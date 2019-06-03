7e64e5ed35cc4c6a2a362476aa833e1c448b3ac1
Lufuno Israel KHUVHE <lkhuvhe@c4r8s2.wethinkcode.co.za>
2019-06-03 18:34:38 +0200

    libft

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,18 +6,34 @@
 /*   By: lkhuvhe <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/28 15:23:14 by lkhuvhe           #+#    #+#             */
-/*   Updated: 2019/05/29 10:37:09 by lkhuvhe          ###   ########.fr       */
+/*   Updated: 2019/06/03 17:37:38 by lkhuvhe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *ft_strstr(const char *haystack, const char *needle)
-{
-	char	*p_needle;
-	char	*p_haystack;
-	int		l;
+#include "libft.h"
 
-	p_needle = (char*)needle;
-	p_haystack = (char*)haystack;
-	l = 0;
-	return (0);
+char	*ft_strstr(const char *haystack, const char *needle)
+{
+	char	*p_h;
+	char	*p_n;
+	int		i;
+	int		j;
+
+	p_n = (char *)needle;
+	p_h = (char *)haystack;
+	if (!(*p_n))
+		return (p_h);
+	i = 0;
+	while (p_h[i] != '\0')
+	{
+		j = 0;
+		while (p_h[i + j] == p_n[j])
+		{
+			if (p_n[j + 1] == '\0')
+				return (&p_h[i]);
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
 }
