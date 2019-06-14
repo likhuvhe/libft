38798893ccfbba7754171ca9243ffee4ead73ae1
Lufuno Israel KHUVHE <lkhuvhe@c4r7s3.wethinkcode.co.za>
2019-06-14 17:13:18 +0200

    libft

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: lkhuvhe <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/28 13:01:10 by lkhuvhe           #+#    #+#             */
-/*   Updated: 2019/05/29 15:21:59 by lkhuvhe          ###   ########.fr       */
+/*   Updated: 2019/06/14 17:00:04 by lkhuvhe          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,10 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 			break ;
 		if (comp1[i] != comp2[i])
 		{
-			return (comp1[i] - comp2[i]);
+			if ((comp1[i] - comp2[i]) < 0)
+				return (-1);
+			else if ((comp1[i] - comp2[i]) > 0)
+				return (1);
 		}
 		else
 		{
